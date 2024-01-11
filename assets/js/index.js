76582af73699458a3bb852cdd7a2c7c21a3ea7fd
danitorres <publicomltda@gmail.com>
2024-01-11 17:25:47 -0300

    Primer Commit

---
+++ assets/js/index.js
@@ -0,0 +1,81 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.scroll1', {
+        duration: 4500,
+        origin: 'top',
+        distance: '-200px'
+
+    });
+    sr.reveal('.scroll2', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-200px'
+
+    });
+    sr.reveal('.scroll3', {
+        duration: 3500,
+        origin: 'top',
+        distance: '-200px'
+
+    });
+    sr.reveal('.scroll4', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-200px'
+
+    });
+    sr.reveal('.scroll5', {
+        duration: 2500,
+        origin: 'top',
+        distance: '-200px'
+
+    });
+    sr.reveal('.scroll6', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-200px'
+
+    });
+
+    sr.reveal('.font-up', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+
+    });
+    sr.reveal('.right', {
+        duration: 4000,
+        origin: 'right',
+        distance: '200px'
+
+    });
+    sr.reveal('.left', {
+        duration: 4000,
+        origin: 'left',
+        distance: '200px'
+
+    });
+    sr.reveal('.card1', {
+        duration: 2500,
+        origin: 'bottom',
+        distance: '-50px'
+
+    });
+    sr.reveal('.card2', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+
+    });
+    sr.reveal('.card3', {
+        duration: 1500,
+        origin: 'bottom',
+        distance: '-50px'
+
+    });
+    sr.reveal('.card4', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-50px'
+
+    });
